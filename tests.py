c5f66d4a91d12456abb789bce4d7cff68e75f3dd
idan054 <idanbit80@gmai.com>
2021-04-16 23:25:29 +0300

    Some mass up version with serverConnector.py files to connect ubuntu18 interpreter (AWS ubuntu18 E)
    V1.0 One main.py file to generate and upload post every 2 hours.

--- tests.py
+++ tests.py
@@ -1,3 +1,22 @@
-import shutil
+import sys
 
-shutil.rmtree('config')  # Delete config folder
+
+def add(a, b):
+  return a + b
+
+
+"""
+You need to execute the script add.py as follows:
+'python add.py 5 2'
+The 'sys.argv[0]' is the name of your script,
+so you need to get the second and the third one.
+"""
+
+print(
+    add(int(sys.argv[1]), int(sys.argv[2]))
+)
+
+"""
+With the command: 'python add.py 5 2',
+this python script will returns 7
+"""
