b34336041c4d5539cbab4b8dd64075065b69effd
idan054 <idanbit80@gmai.com>
2021-04-17 21:38:49 +0300

    V1.2.1 No logs - Generate post from following of the page - clean and separate files - including gDrive connect to export Log file when any error
    V1.1.2 Beta
    cuz unknown reason instabot package not working on Azure/Amazon AWS servers, tested on ubuntu 18+20. python 3.8.
    
    - script Run (main.py) on pythonAnywhere linux server 24/7
    * Already tested on ubuntu20 from Ubuntu Microsoft app
    * All needed funcs in main.py
    * Cleaner version with only needed files
    
    V1.1.0 Some mass up version with serverConnector.py files to connect ubuntu18 interpreter (AWS ubuntu18 E)
    
    V1.0 One main.py file to generate and upload post every 2 hours.

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-import os
+print("New")
+from GlobalGadgets.TelegramPrinter import telegram_printer
 
+telegram_printer(f"WOW. something went wrong throw this upload.\nLuck everything on try except (:")
 
 
