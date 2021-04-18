26796f034c385bcd9b09daeed9fa45b1a9962e78
idan054 <idanbit80@gmai.com>
2021-04-18 23:07:13 +0300

    V1.3 main_sample.py as main - clean ver based separate files - Generate pages based the pages I follow - Send Telegram message on every new upload
    V1.2.1 No logs
    - Generate post from following of the page
    - clean and separate files
    - including gDrive connect to export Log file when any error
    
    V1.1.2 Beta
    cuz unknown reason instabot package not working on Azure/Amazon AWS servers, tested on ubuntu 18+20. python 3.8.
    
    - script Run (main.py) on pythonAnywhere linux server 24/7
    * Already tested on ubuntu20 from Ubuntu Microsoft app
    * All needed funcs in main.py
    * Cleaner version with only needed files
    
    V1.1.0 Some mass up version with serverConnector.py files to connect ubuntu18 interpreter (AWS ubuntu18 E)
    
    V1.0 One main.py file to generate and upload post every 2 hours.

--- main.py
+++ main.py
@@ -4,28 +4,44 @@ from time import sleep
 import datetime
 import schedule
 import sys
+import pytz
 
 ## Values
-from GlobalGadgets.googleDrive import g_drive_upload
+# from GlobalGadgets.googleDrive import g_drive_upload
 
 USERNAME = "spider3d_models"
 PASSWORD = "Idan05423"
 minutesPast_sinceLaunch = 0
 postIndex = 0
+isSample = input("ATTENTION! Make a sample run? (y/n)") or "y"
+
+from Lib.I_CleanStart import clean_start
+## 0. delete config folder & login
+# Only one time bot config
+bot = clean_start(USERNAME, PASSWORD, isSample)  # True / False for is sample upload only? (No insta login)
 
 def main():
+    global isSample
+    # print(isSample)
+    if isSample == "y" or isSample == "Y"\
+                       or isSample == True: # "or True" 2 save next runs
+        isSample = True                     # Change the True to False
+        print("isSample run:", isSample)    # Because its else then y / Y...
+        print("Start Testing mode!")
+    else:
+        isSample = False
+        print("Real LIVE Mode Enabled!")
 
-    current_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    # current_time = datetime.datetime.now().strftime("%d.%m.%Y %H.%M.%S")
+    # start_time = datetime.datetime.now()
+    # start_time = datetime.datetime.now().strftime("%d.%m.%Y %H.%M.%S")
+    start_time = datetime.datetime.now(pytz.FixedOffset(60 * 3))  # (Israel) Every 1 is minute
+    start_time = start_time.strftime("%d/%m/%Y %H:%M:%S")
+    print(start_time)
 
     try:
         print("Start main()")
-
-        from Lib.I_CleanStart import clean_start
-        ## 1. delete config folder & login
-        bot = clean_start(USERNAME, PASSWORD)
-        print(bot)
-
+        global bot
+        print(bot) # When sample: bot = "Sample bot configured!"
 
         from Lib.II_postGenerator import post_generator
         post_data = post_generator(USERNAME)  # Get list of all the pages this user follow on
@@ -47,7 +63,8 @@ def main():
 
         from Lib.III_UploadPost import upload_post
         ## 3. Delete REMOVE_ME & Upload the chosen post
-        post_code = upload_post(bot=bot,
+        post_code = upload_post(sample=isSample,
+                                bot=bot,
                                 photo_link=photo_link,
                                 post_caption=post_caption,
                                 page_username=page_username)  # for credit
@@ -56,33 +73,48 @@ def main():
         postIndex += 1
         telegram_printer(
             f"https://www.instagram.com/p/{post_code}/\n{postIndex} post Already upload since launch ({int(minutesPast_sinceLaunch / 60)} hours ago)")
-        telegram_printer(f"instaBotyy run on pythonAnywhere\n{current_time}")
+        if isSample:
+            telegram_printer(f"SAMPLE MODE instaBotyy run on pythonAnywhere\n{start_time}")
+        else:
+            telegram_printer(f"instaBotyy run on pythonAnywhere\n{start_time}")
     except:
         ## Export failure log to Gdrive & Send Telegram msg
-        telegram_printer(f"WOW. something went wrong throw this upload.\nLuck everything on try except (:")
+        telegram_printer(f"WOW. something went wrong on this upload...")
 
 main()
-schedule.every(10).minutes.do(main)
-# schedule.every(60).to(90).minutes.do(main)
-# schedule.every(2).hours.do(main)
-# schedule.every(10).seconds.do(job)
-# schedule.every(10).minutes.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-# schedule.every().minute.at(":17").do(job)
 
-while True:
-    # looking for pending
-    schedule.run_pending()
-    sleep(60)
-    minutesPast_sinceLaunch += 1
+if isSample:
+    print(isSample)
+    print("Sleep 4 2 sec")
+    sleep(2)
+    main()
+    print("Done. (exit)")
 
-    if minutesPast_sinceLaunch % 10 == 0 : # returns true ONLY if X is an exact multiple of Y (30/10 = True | 3/10 = False)
-        print(f"{minutesPast_sinceLaunch} minutes past. Still alive...", datetime.datetime.now().strftime("%H:%M.%S"))
-        # printYellow(f"{int(whileIndex / 30 + 1)} Posts already uploaded!")
-        print(f"{int(postIndex + 1)} Posts already uploaded!")
+else:
+    print(isSample)
+    schedule.every(60).to(90).minutes.do(main)
+
+
+    # schedule.every(10).seconds.do(main) # Execution every 60 sec
+    # schedule.every(2).hours.do(main)
+    # schedule.every(10).seconds.do(job)
+    # schedule.every(10).minutes.do(job)
+    # schedule.every().hour.do(job)
+    # schedule.every().day.at("10:30").do(job)
+    # schedule.every().monday.do(job)
+    # schedule.every().wednesday.at("13:15").do(job)
+    # schedule.every().minute.at(":17").do(job)
+
+    while True:
+        # looking for pending
+        schedule.run_pending()
+        sleep(6)
+        minutesPast_sinceLaunch += 10
+
+        if minutesPast_sinceLaunch % 10 == 0 : # returns true ONLY if X is an exact multiple of Y (30/10 = True | 3/10 = False)
+            print(f"{minutesPast_sinceLaunch} minutes past. Still alive...", datetime.datetime.now().strftime("%H:%M.%S"))
+            # printYellow(f"{int(whileIndex / 30 + 1)} Posts already uploaded!")
+            print(f"{int(postIndex + 1)} Posts already uploaded!")
 
 
 
